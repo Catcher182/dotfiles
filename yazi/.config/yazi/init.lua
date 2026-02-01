ee22e041b83fb34d55109b69c3e3c93e566a5de6
JinKaijie <k1617486975@outlook.com>
2026-02-01 16:35:26 +0800

    yazi

---
+++ yazi/.config/yazi/init.lua
@@ -0,0 +1,58 @@
+-- 显示git文件的状态
+require("git"):setup()
+-- 跨实例复制
+require("session"):setup({
+  sync_yanked = true,
+})
+-- 智能进入目录或打开文件
+require("smart-enter"):setup({
+  open_multi = true,
+})
+
+Status:children_add(function(self)
+  local h = self._current.hovered
+  if h and h.link_to then
+    return " -> " .. tostring(h.link_to)
+  else
+    return ""
+  end
+end, 3300, Status.LEFT)
+
+-- You can configure your bookmarks by lua language
+local bookmarks = {}
+
+local path_sep = package.config:sub(1, 1)
+local home_path = ya.target_family() == "windows" and os.getenv("USERPROFILE") or os.getenv("HOME")
+if ya.target_family() == "windows" then
+  table.insert(bookmarks, {
+    tag = "Scoop Local",
+    path = (os.getenv("SCOOP") or home_path .. "\\scoop") .. "\\",
+    key = "p",
+  })
+  table.insert(bookmarks, {
+    tag = "Scoop Global",
+    path = (os.getenv("SCOOP_GLOBAL") or "C:\\ProgramData\\scoop") .. "\\",
+    key = "P",
+  })
+end
+table.insert(bookmarks, {
+  tag = "Desktop",
+  path = home_path .. path_sep .. "Desktop" .. path_sep,
+  key = "d",
+})
+
+require("yamb"):setup({
+  -- Optional, the path ending with path seperator represents folder.
+  bookmarks = bookmarks,
+  -- Optional, recieve notification everytime you jump.
+  jump_notify = true,
+  -- Optional, the cli of fzf.
+  cli = "fzf",
+  -- Optional, a string used for randomly generating keys, where the preceding characters have higher priority.
+  keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  -- Optional, the path of bookmarks
+  path = (ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\bookmark")
+      or (os.getenv("HOME") .. "/.config/yazi/bookmark"),
+})
+
+require("gvfs"):setup()
